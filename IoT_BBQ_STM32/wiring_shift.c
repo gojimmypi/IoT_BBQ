976bba2645f9f7982fc13ed55c8ad417f8466a11
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2021-11-24 20:11:03 -0800

    adapted to stm32l4xx_hal

--- IoT_BBQ_STM32/wiring_shift.c
+++ IoT_BBQ_STM32/wiring_shift.c
@@ -4,6 +4,9 @@
 
   Copyright (c) 2005-2006 David A. Mellis
 
+  Adapted from https://github.com/arduino/ArduinoCore-avr/blob/9f8d27f09f3bbd1da1374b5549a82bda55d45d44/cores/arduino/wiring_shift.c
+  by gojimmypi
+  
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
@@ -20,37 +23,53 @@
   Boston, MA  02111-1307  USA
 */
 
-#include "wiring_private.h"
+#include <sys/_stdint.h>
+#include <stm32l4xx_hal.h>
+#define HIGH 0x1
+#define LOW  0x0
+#define LSBFIRST 0
+#define MSBFIRST 1
+
 
 uint8_t shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder) {
-	uint8_t value = 0;
-	uint8_t i;
+    uint8_t value = 0;
+    uint8_t i;
 
-	for (i = 0; i < 8; ++i) {
-		digitalWrite(clockPin, HIGH);
-		if (bitOrder == LSBFIRST)
-			value |= digitalRead(dataPin) << i;
-		else
-			value |= digitalRead(dataPin) << (7 - i);
-		digitalWrite(clockPin, LOW);
-	}
-	return value;
+    for (i = 0; i < 8; ++i) {
+        //digitalWrite(clockPin, HIGH);
+        HAL_GPIO_WritePin(GPIOA, clockPin, HIGH);
+        
+        if (bitOrder == LSBFIRST)
+            //value |= digitalRead(dataPin) << i;
+        value |= HAL_GPIO_ReadPin(GPIOA, dataPin) << i;
+        
+        else
+            value |= HAL_GPIO_ReadPin(GPIOA, dataPin)  << (7 - i);
+        // digitalWrite(clockPin, LOW);
+        HAL_GPIO_WritePin(GPIOA, clockPin, LOW);
+    }
+    return value;
 }
 
 void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val)
 {
-	uint8_t i;
+    uint8_t i;
 
-	for (i = 0; i < 8; i++)  {
-		if (bitOrder == LSBFIRST) {
-			digitalWrite(dataPin, val & 1);
-			val >>= 1;
-		} else {	
-			digitalWrite(dataPin, (val & 128) != 0);
-			val <<= 1;
-		}
-			
-		digitalWrite(clockPin, HIGH);
-		digitalWrite(clockPin, LOW);		
-	}
+    for (i = 0; i < 8; i++) {
+        if (bitOrder == LSBFIRST) {
+            // digitalWrite(dataPin, val & 1);
+            HAL_GPIO_WritePin(GPIOA, dataPin, val & 1);
+            val >>= 1;
+        }
+        else {	
+            // digitalWrite(dataPin, (val & 128) != 0);
+            HAL_GPIO_WritePin(GPIOA, dataPin, (val & 128) != 0);
+            val <<= 1;
+        }
+		
+        //digitalWrite(clockPin, HIGH);
+        //digitalWrite(clockPin, LOW);		
+        HAL_GPIO_WritePin(GPIOA, clockPin, HIGH);
+        HAL_GPIO_WritePin(GPIOA, clockPin, LOW);
+    }
 }
