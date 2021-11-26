c8c6ccdd250cb1bb16eb98a6ab7d91a20cd7251a
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2021-11-26 10:31:23 -0800

    initial C++ wrapper for scale class access from C

---
+++ IoT_BBQ_STM32/task_weight_monitor.cpp
@@ -0,0 +1,15 @@
+#include "task_weight_monitor.h"
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include  "HX711.h"
+    
+    void theScaleTask()
+    {
+        GetScaleWeight();
+    }
+    
+#ifdef __cplusplus
+}
+#endif
---
+++ IoT_BBQ_STM32/task_weight_monitor.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "startup.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    void theScaleTask();
+
+
+#ifdef __cplusplus
+}
+#endif
