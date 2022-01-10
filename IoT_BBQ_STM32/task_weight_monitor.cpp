5e1adb19d86a4b90b42432948f4e1b7582c910bc
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2022-01-09 19:26:11 -0800

    call SSD1306_test() during theScaleTask()

--- IoT_BBQ_STM32/task_weight_monitor.cpp
+++ IoT_BBQ_STM32/task_weight_monitor.cpp
@@ -3,10 +3,16 @@
 extern "C"
 {
 #endif
+
 #include  "HX711.h"
-    
+#include "SSD1306_demo.h"
+
     void theScaleTask()
     {
+        SSD1306_test();
+
+        // ssd1306_TestAll();
+
         GetScaleWeight();
     }
     
