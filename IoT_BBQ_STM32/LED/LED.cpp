ac4a11c6097fa2ffdc528fe72af565af0010eba8
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2022-01-30 16:26:35 -0800

    SetAppState(Running) during IsBlinking

--- IoT_BBQ_STM32/LED/LED.cpp
+++ IoT_BBQ_STM32/LED/LED.cpp
@@ -1,5 +1,6 @@
 #include "../LED/LED.h"
 #include <stm32l4xx_hal.h>
+#include <Tasks/state_machine.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -23,6 +24,16 @@ extern "C" {
     {
         // TODO RTOS wrap
         __current_LED_MODE = NewState;
+        
+        // TODO we probably don't want to set application runnnig state based on LED
+        if (__current_LED_MODE == IsBlinking)
+        {
+            SetAppState(Running);
+        }
+        else
+        {
+            SetAppState(Demo);
+        }
         
         return 0; // TODO check for errors
     }
@@ -133,6 +144,77 @@ extern "C" {
     }
 
 
+    void temp_MX_TIM1_Init(void)
+    {
+
+        TIM_MasterConfigTypeDef sMasterConfig;
+        TIM_OC_InitTypeDef sConfigOC;
+        TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig;
+        
+        TIM_HandleTypeDef htim1;
+        
+        htim1.Instance = TIM1;
+        htim1.Init.Prescaler = 80;
+        htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+        htim1.Init.Period = 20000;
+        htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+        htim1.Init.RepetitionCounter = 0;
+        htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+        if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+        {
+            //_Error_Handler(__FILE__, __LINE__);
+        }
+
+        sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+        sMasterConfig.MasterOutputTrigger2 = TIM_TRGO2_RESET;
+        sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+        if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+        {
+           // _Error_Handler(__FILE__, __LINE__);
+        }
+
+        sConfigOC.OCMode = TIM_OCMODE_PWM1;
+        sConfigOC.Pulse = 1000;
+        sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+        sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+        sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+        sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+        sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+        if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+        {
+           // _Error_Handler(__FILE__, __LINE__);
+        }
+
+        if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+        {
+           // _Error_Handler(__FILE__, __LINE__);
+        }
+
+        if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
+        {
+            //_Error_Handler(__FILE__, __LINE__);
+        }
+
+        sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+        sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+        sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+        sBreakDeadTimeConfig.DeadTime = 0;
+        sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+        sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+        sBreakDeadTimeConfig.BreakFilter = 0;
+        sBreakDeadTimeConfig.Break2State = TIM_BREAK2_DISABLE;
+        sBreakDeadTimeConfig.Break2Polarity = TIM_BREAK2POLARITY_HIGH;
+        sBreakDeadTimeConfig.Break2Filter = 0;
+        sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+        if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+        {
+            //_Error_Handler(__FILE__, __LINE__);
+        }
+
+        HAL_TIM_Base_MspInit(&htim1);
+
+    }
+    
 #ifdef __cplusplus
 }
 #endif    
