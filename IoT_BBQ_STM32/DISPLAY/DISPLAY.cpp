893c82cd2b8e9fa53a320fcee0b7216d85b134e3
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2022-01-30 16:27:18 -0800

    UART_TxMessage for scale value

--- IoT_BBQ_STM32/DISPLAY/DISPLAY.cpp
+++ IoT_BBQ_STM32/DISPLAY/DISPLAY.cpp
@@ -1,8 +1,15 @@
+#include <cmsis_os.h>
+
 #include "DISPLAY.h"
 #include <stm32l4xx_hal.h>
+#include <SSD1306/ssd1306.h>
+#include <SSD1306/SSD1306_demo.h>
+#include <UART/UART.h>
+#include "Tasks/state_machine.h"
+#include "Tasks/task_weight_monitor.h"
+#include "DISPLAY/DISPLAY.h"
 
 #ifdef __cplusplus
-#include <SSD1306/SSD1306_demo.h>
 
 extern "C" {
 #endif
@@ -13,10 +20,30 @@ extern "C" {
 
     void DISPLAY_Thread1(void const* argument) {
         (void)argument;
-
+        static const TickType_t xDelay = 500 / portTICK_PERIOD_MS;
+        static long CurrentTankWeight = 0;
+        static const int bufferLenth = 20;
+        static uint8_t WeightMessage[bufferLenth] = "Weight = ";
+        static uint8_t CrLf[bufferLenth] = "\n\r";
+       
         for (;;)
         {
-            SSD1306_test();
+            switch (GetAppState())
+            {
+            case Running:
+                ssd1306_Fill(Black);
+                CurrentTankWeight = GetScaleWeight();
+                UART_TxMessageIntValueHex(WeightMessage, bufferLenth, CurrentTankWeight);
+                UART_TxMessage(CrLf, bufferLenth);
+                osDelay(xDelay); 
+                
+                break;
+                
+            case Demo:
+            default:
+                ssd1306_TestAll();
+                break;
+            }
         }
     }
     
