cdef10e8e66d7526bb34e755147e5f5d64c788a8
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2022-02-04 18:51:07 -0800

    don't osDelay() inside xSemaphoreTake

--- IoT_BBQ_STM32/Tasks/task_weight_monitor.cpp
+++ IoT_BBQ_STM32/Tasks/task_weight_monitor.cpp
@@ -13,53 +13,61 @@ extern "C"
     // SemaphoreHandle_t xHX711_Semaphore = NULL;
     SemaphoreHandle_t xHX711_Semaphore = xSemaphoreCreateMutex();
     static long TheScaleWeight = 0;
-    static bool IsInitialized = false;
+    static bool _IsInitialized = false;
+    
     
     static HX711 scale; 
     
+    int GetScaleIsInitialized()
+    {
+        return _IsInitialized;
+    }
+    
     long GetScaleWeight()
     {
+        /* We were able to obtain the semaphore and can now access the
+        shared resource. */
+
+        /* ... */
+        if (_IsInitialized)
+        {
+            // the scale is already initialized
+        }
+        else 
+        {
+            scale.power_down();
+            osDelay(5000);
+            
+            scale.begin(0, 0); // TODO params not current used! hard coded
+
+            _IsInitialized = true;
+
+            // calibrate
+            int DoCalibrate = 0;
+            if (DoCalibrate == 1)
+            {
+                scale.set_scale();
+                    
+                // Step 2 Call tare() with no parameter.
+                scale.tare();
+
+                // Step 3 Place a known weight on the scale and call get_units(10).
+                float a = scale.get_units(10);
+
+                // Divide the result in step 3 to your known weight.You should get about the parameter you need to pass to set_scale().
+                float theValue = a / 2;
+                    
+                scale.set_scale(theValue);
+            }
+            // Step 1 Call set_scale() with no parameter.
+        }
+                
         if (xHX711_Semaphore != NULL)
         {
             /* See if we can obtain the semaphore.  If the semaphore is not
             available wait 10 ticks to see if it becomes free. */
             if (xSemaphoreTake(xHX711_Semaphore, (TickType_t) 10) == pdTRUE)
             {
-                /* We were able to obtain the semaphore and can now access the
-                shared resource. */
-
-                /* ... */
-                if (IsInitialized)
-                {
-                    // the scale is already initialized
-                }
-                else 
-                {
-                    scale.power_down();
-                    osDelay(5000);
-                    scale.begin(0, 0); // TODO params not current used! hard coded
-                    IsInitialized = true;
-
-                    // calibrate
-                    int DoCalibrate = 0;
-                    if (DoCalibrate == 1)
-                    {
-                        scale.set_scale();
-                    
-                        // Step 2 Call tare() with no parameter.
-                        scale.tare();
-
-                        // Step 3 Place a known weight on the scale and call get_units(10).
-                        float a = scale.get_units(10);
-
-                        // Divide the result in step 3 to your known weight.You should get about the parameter you need to pass to set_scale().
-                        float theValue = a / 2;
-                    
-                        scale.set_scale(theValue);
-                        }
-                    // Step 1 Call set_scale() with no parameter.
-                }
-                
                 TheScaleWeight = scale.get_units(10);
 
 
